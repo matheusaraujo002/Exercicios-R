3e7cdcdabd045b70594ec6efa9af5685633b6292
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:19:25 -0300

    Criando sequencias de números com seq()

---
+++ Lista2/1.sequencia.R
@@ -0,0 +1,16 @@
+################################################
+# Exercício 1
+
+# Usando o conceito e códigos de sequência numérica de valores, crie as sequências abaixo no RStudio:
+# a) sequência de números de 1 a 5.
+# b) sequência de números de 5 a 1.
+# c) sequência de números de 1 a 20, com incremento de 5.
+# d) sequência de números de 20 a 1, com incremento de 5.
+# e) sequência de números de 20 a 1, com incremento de -5.
+
+
+print(seq(1, 5))
+print(seq(5, 1))
+print(seq(1, 20, 5))
+print(seq(20, 1, -5))
+print(seq(20, 1, -5))
