9cf2538ea4edd1056fe8da3d6a16c51239a4cfda
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:13:50 -0300

    Arredondando decimais com ceiling e floor

---
+++ Lista1/3.arredondamento.R
@@ -0,0 +1,15 @@
+################################################
+# Exercício 3
+
+# Faça os arredondamentos requeridos usando das funções ceiling (x) e floor (x) e 
+# transcreva a sequência os comandos utilizados até chegar ao resultado.
+
+# a) Arredonde o valor de 2,547 para o maior valor.
+# b) Arredonde o valor de 2,547 para o menor valor.
+
+a = 2.547
+b = 2.547
+
+print('Exercício 3:')
+print(ceiling(a))
+print(floor(b))
