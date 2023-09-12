1003cfee1617fa41723aae868ed0e6de6a8a7765
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:32:15 -0300

    Criando um array

---
+++ Lista2/6.array.R
@@ -0,0 +1,8 @@
+# Crie um array com sequência de números de 1 a 16, com 4 linhas, 2 colunas, 
+# divididos em 2 matrizes. Após isso, localize o elemento da segunda linha e 
+# segunda coluna da matriz 2.
+
+y = array(1:16,c(4,2,2))
+print(y)
+
+print(y[2,2,2])
