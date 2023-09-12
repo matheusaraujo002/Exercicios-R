a291bda247f46c75e5614a157e50f6156fd0f6c9
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:17:24 -0300

    Definindo e removendo uma variável

---
+++ Lista1/6.variaveis.R
@@ -0,0 +1,13 @@
+################################################
+# Exercício 6
+
+# Atribua o valor de 25 para a variável x e o valor 17 para a variável y. 
+# Em seguida dê o comando para remover a variável x. Transcreva os comandos 
+# que aparecem no console abaixo desta questão.
+
+print('Exercício 6:')
+x = 25
+y = 17
+
+print('Removendo x...')
+rm(x)
