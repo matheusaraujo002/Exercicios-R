d2852521de85f4d7c6392165aeef5423374c95b9
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:16:39 -0300

    Arredondando decimais significativos com signif()

---
+++ Lista1/5.significativos.R
@@ -0,0 +1,17 @@
+################################################
+# Exercício 5
+
+# Arredonde os valores de acordo com o número pré-estabelecido de algarismos 
+# significativos. Consulte a função para esta atividade e transcreva a sequência 
+# os comandos que aparecem no console, que foram utilizados até chegar ao 
+# resultado.
+
+# a) 2,4785269 para 3 algarismos significativos.
+# b) 84,5468 para 2 algarismos significativos.
+# c) 5,8467 para 1 algarismo significativo.
+
+print()
+print('Exercício 5:')
+print(signif(2.4785269, 3))
+print(signif(84.5468, 3))
+print(signif(5.8467, 3))
