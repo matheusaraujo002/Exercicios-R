a257a2fa38c98f09b4b14510e296fc063a578080
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:14:49 -0300

    Arredondando decimais com round

---
+++ Lista1/4.decimal.R
@@ -0,0 +1,15 @@
+################################################
+# Exercício 4
+
+# Arredonde os valores de acordo com o número pré-estabelecido de casas decimais. 
+# Consulte a função para esta atividade e transcreva a sequência dos comandos 
+# utilizados até chegar ao resultado.
+
+# a) 2,4785269 para 3 casas decimais.
+# b) 84,5468 para 2 casas decimais.
+# c) 5,8467 para 1 casa decimal.
+
+print('Exercício 4:')
+print(round(2.4785269,digits = 3))
+print(round(84.5468,digits = 2))
+print(round(5.8467,digits = 1))
