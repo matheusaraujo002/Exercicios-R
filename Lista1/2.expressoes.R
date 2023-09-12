09a1d14a489ac80d51e506c4d616355582bf060b
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:11:50 -0300

    Expressões numéricas

---
+++ Lista1/2.expressoes.R
@@ -0,0 +1,29 @@
+################################################
+# Exercício 2
+
+# Digite as expressões numéricas no script do R e encontre os resultados.
+
+# a) 4+9
+# b) 4-5
+# c) 4x5
+# d) 4:5
+# e) 4²
+# f) 4^5
+# g) seno de 45°
+# h) (25-12) ²
+# i) (4+5)x7-(36/18)³
+# j) 0/0
+# k) Inf/0
+
+print('Exercício 2:')
+print(4+9)
+print(4-5)
+print(4*5)
+print(4/5)
+print(4**2)
+print(4**5)
+print(sin(45))
+print((25-12)**2)
+print((4+5)*7-(36/18)**3)
+print(0/0)
+print(Inf/0)
