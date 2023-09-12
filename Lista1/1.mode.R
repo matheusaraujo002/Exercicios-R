05f5c13b31de329f4135ad42012dc36974e85fca
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-09-12 18:10:58 -0300

    Função mode()

---
+++ Lista1/1.mode.R
@@ -0,0 +1,18 @@
+################################################
+# Exercício 1
+
+# Encontrar o tipo de dado em cada questão digitando os códigos abaixo no script do R, 
+# utilizando o código mode(.......). 
+
+# Exiba os comandos utilizados até chegar ao resultado.
+
+# a) valor
+# b) string
+# c) 2<4
+# d) sin
+
+print('Exercício 1:')
+print(mode(90))
+print(mode('teste'))
+print(mode(2 < 4))
+print(mode(sin(2)))
